ae2606df5e3f3b0c2d4f85cf2f36492e3bc3a6d7
M4RIGNAL-1981 <m4rginal@mail.ru>
2025-10-18 02:31:23 +0500

    Изменения в LB4

--- LB/LB4.py
+++ LB/LB4.py
@@ -1,4 +1,6 @@
 import numpy as np
+import math
+import matplotlib.pyplot as plt
 import multiprocessing as mp
 import time 
 
@@ -6,28 +8,35 @@ class F:
     def __call__(self, x):
         return np.sqrt(1 - x**2)
     
+    def S(self):
+        return math.pi / 2
+    
     def __str__(self):
         return "sqrt(1 - x**2)"
     
-def MonteKarlo(a: int, b: int, f, N: int):
+
+def MonteKarloGeo(a: int, b: int, c: int, d: int, f, N: int):
     curva_x = np.random.uniform(a, b, N)
-    return (b - a) * np.mean(f(curva_x))
+    curva_y = np.random.uniform(c, d, N)
+    curva_G = curva_y <= f(curva_x)
+    return (b - a) * (d - c) * np.mean(curva_G)
+
 
 # Функция для worker должна быть на верхнем уровне
 def worker_function(args):
-    a_local, b_local, f_local, points = args
-    return MonteKarlo(a_local, b_local, f_local, points)
+    a_local, b_local, c_local, d_local, f_local, points = args
+    return MonteKarloGeo(a_local, b_local, c_local, d_local, f_local, points)
 
-def ParallelMonteCarloSUM(a: int, b: int, f, N: int, num_processes=4):
+def ParallelMonteCarloSUM(a: int, b: int, c: int, d: int, f, N: int, num_processes=4):
     if N <= int(1e8):
-        return MonteKarlo(a, b, f, N)
+        return MonteKarloGeo(a, b, c, d, f, N)
     
     points_per_process = N // num_processes
     
     print(f"Параллельный расчет на {num_processes} процессах")
     print(f"Всего точек: {N:,}, на процесс: {points_per_process:,}")
     
-    tasks = [(a, b, f, points_per_process) for _ in range(num_processes)]
+    tasks = [(a, b, c, d, f, points_per_process) for _ in range(num_processes)]
     
     start_time = time.time()
     with mp.Pool(processes=num_processes) as pool:
@@ -38,26 +47,45 @@ def ParallelMonteCarloSUM(a: int, b: int, f, N: int, num_processes=4):
     
     return result
 
-def MonteKarloGeometric(a: int, b: int, c: int, d: int, f, N: int):
-    curva_x = np.random.uniform(a, b, N)
-    curva_y = np.random.uniform(c, d, N)
-    curva_G = curva_y <= f(curva_x)
-    return (b - a) * (d - c) * np.mean(curva_G)
+
+def Print(N_values, Results_values, f):
+    plt.figure(figsize=(8, 6))
+    plt.plot(N_values, Results_values - f.S, linewidth=2, label='Cходимость')
+    plt.plot(N_values, f.S, linewidth=2, label='Аналитическая площадь')
+
+    plt.title(f"Анализ сходимости")
+    plt.xlabel("N")
+    plt.ylabel("Площадь A")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+def PrintTime(N_values, Time_values):
+    plt.figure(figsize=(8, 6))
+    plt.plot(N_values, Time_values, linewidth=2, label='Зависимость кол-во времени от кол-во точек')
+
+    plt.title(f"Анализ сходимости")
+    plt.xlabel("N")
+    plt.ylabel("Кол-во времени")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
 
 if __name__ == "__main__":
     A = -1
     B = 1
     C = 0
     D = 1
-    N = int(1e9)
+    N_values = [1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9]
+    Time_values = []
+    Results_values = []
     f = F()
 
-    start = time.time()
-
-    result = ParallelMonteCarloSUM(A, B, f, N, num_processes=4)
-    
-    end = time.time()
-
-    print(f"A: {A}, B: {B}, f: {f}, N: {N}")
-    print(f"S: {result}")
-    print(f"Прошло {round((end - start) / 60, 2)} минут")
+    for i in range(len(N_values)):
+        start = time.time()
+        Results_values.append(ParallelMonteCarloSUM(A, B, C, D, f, N_values[i], num_processes=4))
+        end = time.time()
+        Time_values.append(round((end - start) / 60))
+        
+    Print(N_values, Results_values, f)
+    PrintTime(N_values, Time_values)
